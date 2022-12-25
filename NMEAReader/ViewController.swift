64272499d406273ec34e0f419bb90e02571d0df0
bacataBorisov <112765301+bacataBorisov@users.noreply.github.com>
2022-12-25 16:04:59 +0100

    Initial Commit

---
+++ NMEAReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NMEAReader
+//
+//  Created by Vasil Borisov on 25.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
